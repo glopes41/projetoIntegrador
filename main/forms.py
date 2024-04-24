d6bb6982361e7d74f6843c9191dc7286e2ec5a79
glopes41 <g.lopes41@gmail.com>
2024-04-24 11:44:22 -0300

    Ajustado o formulario avaliação para pegar a data e o número do concurso automaticamente.

--- main/forms.py
+++ main/forms.py
@@ -1,26 +1,30 @@
 from django import forms
 from .models import Prova, Avaliacao, Tipo, Concurso
 
+
 class FormAvaliacao(forms.ModelForm):
-    tipo_prova = forms.ModelChoiceField(label='Tipo da Prova', queryset=Tipo.objects.all())
-    concurso = forms.ModelChoiceField(label='Concurso', queryset=Concurso.objects.all())
-    
+    tipo_prova = forms.ModelChoiceField(
+        label='Tipo da Prova', queryset=Tipo.objects.all())
+
     class Meta:
         model = Avaliacao
-        fields = ['nota', 'data', 'candidato', 'avaliacao']
+        fields = ['nota', 'candidato', 'avaliacao']
 
     def save(self, commit=True):
         instance = super().save(commit=False)
-        
+
+        # Instancia concurso
+        concurso = Concurso.objects.first()
         # Obtendo os dados do formulário
         tipo_prova_descricao = self.cleaned_data['tipo_prova']
-        concurso = self.cleaned_data['concurso']
 
+        instance.data = concurso.data
         # Verificando se a instância já tem uma prova associada
         if not instance.prova_id:
             # Criando a Prova apenas se não houver uma associada à Avaliação
             tipo_prova_obj = Tipo.objects.get(descricao=tipo_prova_descricao)
-            nova_prova = Prova.objects.create(tipo=tipo_prova_obj, concurso=concurso)
+            nova_prova = Prova.objects.create(
+                tipo=tipo_prova_obj, concurso=concurso)
             instance.prova = nova_prova
 
         if commit:
--- projetoIntegrador/settings.py
+++ projetoIntegrador/settings.py
@@ -127,9 +127,9 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static" )
+    os.path.join(BASE_DIR, "static")
 ]
 
 TEMPLATE_DIRS = [
-os.path.join(BASE_DIR, "templates")
+    os.path.join(BASE_DIR, "templates")
 ]
